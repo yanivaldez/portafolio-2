31f94c82c6c4072e36ddc26e511638b91950ef3d
Yanivaldez <yanitzavaldez@gmail.com>
2023-01-11 22:41:29 -0300

    guardando cambios

--- assets/js/productos.js
+++ assets/js/productos.js
@@ -4,7 +4,7 @@ let productos= [
     nombre: "Tortas para 10 personas",
     descripción: "Tamaño 15 cm",
     precio: 45.000,
-    descuento: 10%
+    descuento: 1000,
     imagen: "./assets/img/IMG-20221209-WA0062.jpg"
 },
 
@@ -13,15 +13,15 @@ let productos= [
     nombre: "Tortas para 15 personas",
     descripción: "Tamaño 18 cm",
     precio: 55.000,
-    descuento: 10%
-    imagen: "./assets/img/IMG-20221126-WA0000.jpg"
+    descuento: 1000,
+    imagen: "./assets/221126-WA0000.jpg"
 },
 {
     sku: "0003",
     nombre: "Tortas para 20 personas",
     descripción: "Tamaño 20 cm",
     precio: 65.000,
-    descuento: 10%
+    descuento: 1000,
     imagen: "./assets/img/AGEQ3526.JPEG"
 },
 {
@@ -29,7 +29,7 @@ let productos= [
     nombre: "Tortas para 25 personas",
     descripción: "Tamaño 22 cm",
     precio: 75.000,
-    descuento: 10%
+    descuento: 1000,
     imagen: "./assets/img/HSPB7971.JPEG"
 },
 {
@@ -37,7 +37,7 @@ let productos= [
     nombre: "Tortas para 25 personas",
     descripción: "Tamaño 15 y 18 cm",
     precio: 80.000,
-    descuento: 10%
+    descuento: 1000,
     imagen: "./assets/img/IMG-20221210-WA0031.jpg"
 },
 {
@@ -45,7 +45,7 @@ let productos= [
     nombre: "Tortas para 45 personas",
     descripción: "Tamaño 12, 16 y 20 cm",
     precio: 140.000,
-    descuento: 10%
+    descuento: 1000,
     imagen: "./assets/img/IMG-20221204-WA0000.jpg"
 },
 {
@@ -53,7 +53,7 @@ let productos= [
     nombre: "Tortas para 80 personas",
     descripción: "Tamaño 12, 16, 20 y 24 cm",
     precio: 240.000,
-    descuento: 10%
+    descuento: 1000,
     imagen: "./assets/img/IMG-20221209-WA0148.jpg"
 },
 {
@@ -61,7 +61,7 @@ let productos= [
     nombre: "Tortas para 120 personas",
     descripción: "Tamaño 12, 16, 20, 24 y 30 cm",
     precio: 360.000,
-    descuento: 10%
+    descuento: 1000,
     imagen: "./assets/img/IMG-20221218-WA0014.jpg"
 },
 ]
